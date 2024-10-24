43beb9fbf402726a3c9b9b5d28dfdffea2bc234b
Karen <karen.millena@alunoescolassociais.org.br>
2024-10-24 08:10:42 -0300

    adiciona js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('currency-form').addEventListener('submit', function(event){
+    event.preventDefault();
+
+
+
+
+
+
+    // Obter valores de entrada do formulário
+  const valor = parseFloat(document.getElementById('a'))
+})
